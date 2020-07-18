9a05ddf7dc6d936f963ff2cbba9a24e9de2fe4d7
wenchao.zhang_U+ <wenchao.zhang@united-imaging.com>
2020-07-18 13:53:09 +0800

    add

--- JavaScript/diy.js
+++ JavaScript/diy.js
@@ -1,3 +1,15 @@
+// 箭头函数是普通函数的简写，可以更优雅的定义一个函数，和普通函数相比，有以下几点差异：
+
+// 1、函数体内的 this 对象，就是定义时所在的对象，而不是使用时所在的对象。
+
+// 2、不可以使用 arguments 对象，该对象在函数体内不存在。如果要用，可以用 rest 参数代替。
+
+// 3、不可以使用 yield 命令，因此箭头函数不能用作 Generator 函数。
+
+// 4、不可以使用 new 命令，因为：
+
+// 没有自己的 this，无法调用 call，apply。
+// 没有 prototype 属性 ，而 new 命令在执行时需要将构造函数的 prototype 赋值给新的对象的 __proto__
 Array.prototype.myForeach=function(fun) {
   for (let i=0; i < this.length; i++) {
     fun.call(this,this[i], i,this)
@@ -40,6 +52,36 @@ Array.prototype.myRightReduce = function(fun,initValue) {
   }
   return result;
 }
+
+Function.prototype.myCall = function(obj) {
+  const context = obj || window;
+  context.func = this;
+  const arg = Array.from(arguments).slice(1)
+  const res = arg.length > 0 ? context.func(...arg) : context.func();
+  delete context.func;
+  return res;
+}
+
+Function.prototype.myApply = function(obj) {
+  const context = obj || window;
+  context.func = this;
+  const arg = Array.from(arguments).slice(1)
+  const res = arg.length > 0 ? context.func(arg) : context.func();
+  delete context.func;
+  return res;
+}
+
+Function.prototype.myBind = function(obj) {
+  const context = obj || window;
+  context.func = this;
+  const arg = Array.from(arguments).slice(1);
+  return function () {
+    const arg1 = Array.from(arguments);
+    const allArgs = arg.concat(arg1);
+    return allArgs.length > 0 ? context.func(...allArgs) : context.func()
+  }
+}
+
 
 class Promise{
   constructor(executor){
@@ -207,4 +249,90 @@ Promise.all = function(promises){
       },reject);
     };
   });
+}
+
+function bubbleSort (arr) {
+  for(var i =0; i< arr.length; i++) {
+    for(var j=i; j<arr.length; j++) {
+      if (arr[i]>arr[j]) {
+        var tem = arr[i]
+        arr[i] = arr[j];
+        arr[j] = tem;
+      }
+    }
+  }
+  return arr
+}
+
+function bigNumberAdd(a, b) {
+  var str1 = a.split("").reverse();
+  var str2 = b.split("").reverse();
+  var tem = 0;
+  var addFlag = 0;
+  var bigger = [];
+  var little = [];
+  var res = [];
+  if (parseInt(a) > parseInt(b)) {
+    bigger = str1;
+    little = str2;
+  } else {
+    bigger = str2;
+    little = str1;
+  }
+  for (var i = 0; i< bigger.length; i++) {
+    if (i < little.length) {
+      tem = parseInt(bigger[i]) + parseInt(little[i]) + addFlag;
+    } else {
+      tem = parseInt(bigger[i]) + addFlag;
+    }
+    if (tem < 10) {
+      res[i] = tem;
+      addFlag = 0;
+    } else {
+      res[i] = tem - 10;
+      addFlag = 1;
+      if (i == bigger.length -1) {
+        res[bigger.length] =1;
+      }
+    }
+  }
+  return res.reverse().join("");
+}
+
+function add(a,b){
+  var str1,str2,temp;
+  var addFlag = 0;
+  var max = [],min = [],res = [];
+  str1 = a.split("").reverse();//分割字符串，并且反转
+  str2 = b.split("").reverse();
+
+  if (parseInt(a) >= parseInt(b)) {
+      max = str1;
+      min = str2;
+  }
+  else{
+      max = str2;
+      min = str1;
+  }
+  for (var i = 0; i <= max.length - 1; i++) {
+      if (i <= min.length - 1) {
+          temp = parseInt(max[i]) + parseInt(min[i]) + addFlag;
+      }
+      else{
+          temp = parseInt(max[i]) + addFlag;
+      }
+
+      if (temp > 9) {
+          res[i] = temp - 10;
+          addFlag = 1;
+          if (i == max.length - 1) {
+              res[max.length] = 1;//如果是最后一位，要进位
+          }
+      }
+      else{
+          res.push(temp);
+          addFlag = 0;
+      }
+  };
+  return res.reverse().join("");
 }
