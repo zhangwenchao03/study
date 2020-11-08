68a9107bcb04c740aba576c566016b94ef78fe77
zhangwenchao03 <1570416024@qq.com>
2020-11-08 10:06:21 +0800

    update

--- JavaScript/diy.js
+++ JavaScript/diy.js
@@ -424,4 +424,26 @@ function quickSort(items, left, right) {
   }
   return items;
 }
+// 快速排序
+// 选择基准值(base)，原数组长度减一(基准值)，使用 splice
+// 循环原数组，小的放左边(left数组)，大的放右边(right数组);
+// concat(left, base, right)
+// 递归继续排序 left 与 right
+
+function quickSort(arr) {
+  if(arr.length <= 1) {
+      return arr;  //递归出口
+  }
+  var left = [],
+      right = [],
+      current = arr.splice(0,1); 
+  for(let i = 0; i < arr.length; i++) {
+      if(arr[i] < current) {
+          left.push(arr[i])  //放在左边
+      } else {
+          right.push(arr[i]) //放在右边
+      }
+  }
+  return quickSort(left).concat(current,quickSort(right));
+}
 
